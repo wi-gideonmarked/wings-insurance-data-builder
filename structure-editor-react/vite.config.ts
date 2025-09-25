7ac911e42487fa1521371802c1d4496fae30fa65
Gideon Mark Pacete <gideonmarkcpacete@gmail.com>
2025-09-25 22:22:18 +0800

    0.0.0.0 updates

--- structure-editor-react/vite.config.ts
+++ structure-editor-react/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Allow external connections
     port: 3000, // or any other port you prefer
     fs: {
       allow: ['..']
@@ -25,6 +26,7 @@ export default defineConfig({
     }
   },
   preview: {
+    host: '0.0.0.0', // Allow external connections
     port: 80,
     strictPort: true
   }
